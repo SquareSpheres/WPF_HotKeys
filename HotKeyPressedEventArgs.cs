d1c5036c1ed8c6a2f8deb213e65e7e91b025649f
Yngve <yngvenot@gmail.com>
2017-10-15 20:33:22 +0200

    Added modifiers to eventArgs

--- HotKeyPressedEventArgs.cs
+++ HotKeyPressedEventArgs.cs
@@ -5,14 +5,15 @@ namespace HotKeysLib
 {
     public class HotKeyPressedEventArgs : EventArgs
     {
-        
-        public HotKeyPressedEventArgs(int ptX, int ptY, int time, int hotKeyId, VirtualKeys key)
+
+        public HotKeyPressedEventArgs(int ptX, int ptY, int time, int hotKeyId, VirtualKeys key, Modifiers modifiers)
         {
             PtX = ptX;
             PtY = ptY;
             Time = time;
             Key = key;
             HotKeyId = hotKeyId;
+            Modifiers = modifiers;
         }
 
         public int PtX { get; }
@@ -20,7 +21,8 @@ namespace HotKeysLib
         public int Time { get; }
         public int HotKeyId { get; }
         public VirtualKeys Key { get; }
-      
+        public Modifiers Modifiers { get; }
+
     }
 }
 
