03c69d48573759fd78cad1ba20a394afe0265b37
Yngve <yngvenot@gmail.com>
2017-10-15 20:33:07 +0200

    Changed location for MOD_NOREPEAT

--- VKeys.cs
+++ VKeys.cs
@@ -10,12 +10,13 @@ namespace HotKeysLib
         MOD_ALT = 0x0001,
         //Either CTRL key must be held down.
         MOD_CONTROL = 0x0002,
-        //Changes the hotkey behavior so that the keyboard auto-repeat does not yield multiple hotkey notifications.
-        MOD_NOREPEAT = 0x4000,
         //Either SHIFT key must be held down.
         MOD_SHIFT = 0x0004,
         //Either WINDOWS key was held down. These keys are labeled with the Windows logo. Keyboard shortcuts that involve the WINDOWS key are reserved for use by the operating system.
         MOD_WIN = 0x0008,
+        //Changes the hotkey behavior so that the keyboard auto-repeat does not yield multiple hotkey notifications.
+        MOD_NOREPEAT = 0x4000,
+
 
     }
 
