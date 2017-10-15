b512ee4b2b454a4601b207793d890faabd197be2
Yngve <yngvenot@gmail.com>
2017-10-15 16:39:19 +0200

    Added modifiers

--- VKeys.cs
+++ VKeys.cs
@@ -1,5 +1,22 @@
-﻿namespace HotKey
+﻿namespace HotKeysLib
 {
+
+    public enum Modifiers
+: uint
+    {
+        //Either ALT key must be held down.
+        MOD_ALT = 0x0001,
+        //Either CTRL key must be held down.
+        MOD_CONTROL = 0x0002,
+        //Changes the hotkey behavior so that the keyboard auto-repeat does not yield multiple hotkey notifications.
+        MOD_NOREPEAT = 0x4000,
+        //Either SHIFT key must be held down.
+        MOD_SHIFT = 0x0004,
+        //Either WINDOWS key was held down. These keys are labeled with the Windows logo. Keyboard shortcuts that involve the WINDOWS key are reserved for use by the operating system.
+        MOD_WIN = 0x0008,
+
+    }
+
     /// <summary>
     /// Enumeration for virtual keys.
     /// http://www.pinvoke.net/default.aspx/Enums.VirtualKeys
