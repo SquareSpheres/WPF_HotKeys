806a07033e600ccadabeef9dd00288e60e1beb8c
Yngve <yngvenot@gmail.com>
2017-10-15 17:56:39 +0200

    EventArgs for hotkey

---
+++ HotKeyPressedEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HotKeysLib
+
+{
+    public class HotKeyPressedEventArgs : EventArgs
+    {
+        
+        public HotKeyPressedEventArgs(int ptX, int ptY, int time, int hotKeyId, VirtualKeys key)
+        {
+            PtX = ptX;
+            PtY = ptY;
+            Time = time;
+            Key = key;
+            HotKeyId = hotKeyId;
+        }
+
+        public int PtX { get; }
+        public int PtY { get; }
+        public int Time { get; }
+        public int HotKeyId { get; }
+        public VirtualKeys Key { get; }
+      
+    }
+}
+
