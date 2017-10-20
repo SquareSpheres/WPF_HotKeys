8a08f1e2e12641910dbf3ea3ac49b8d35dd83871
Yngve <yngvenot@gmail.com>
2017-10-20 17:41:43 +0200

    Imported more functions from winAPI, and created a fucntion to list all active windows

---
+++ WinUtils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotKeysLib
+{
+    public static class WinUtils
+    {
+
+        private static readonly Dictionary<string, IntPtr> ActiveWindows = new Dictionary<string, IntPtr>();
+
+        private static bool EnumTheWindows(IntPtr hWnd, IntPtr lParam)
+        {
+
+            int windowNameSize = WindowsFunctions.GetWindowTextLength(hWnd);
+
+            if (hWnd == WindowsFunctions.GetShellWindow()) return true;
+            if (windowNameSize == 0) return true;
+            if (!WindowsFunctions.IsWindowVisible(hWnd)) return true;
+
+
+            StringBuilder sb = new StringBuilder(windowNameSize + 1);
+            WindowsFunctions.GetWindowText(hWnd, sb, windowNameSize + 1);
+
+            ActiveWindows.Add(sb.ToString(), hWnd);
+
+            return true;
+        }
+
+        private static void UpdateDictionary()
+        {
+            ActiveWindows.Clear();
+            WindowsFunctions.EnumWindows(EnumTheWindows, IntPtr.Zero);
+        }
+
+        public static List<string> GetActiveWindowNames()
+        {
+            UpdateDictionary();
+            return ActiveWindows.Keys.ToList();
+        }
+
+        public static IntPtr GetWindowHandle(string windowName)
+        {
+            UpdateDictionary();
+            return ActiveWindows.ContainsKey(windowName) ? ActiveWindows[windowName] : IntPtr.Zero;
+        }
+    }
+}
--- WindowsFunctions.cs
+++ WindowsFunctions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace HotKeysLib
 {
@@ -9,5 +10,19 @@ namespace HotKeysLib
         public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vlc);
         [DllImport("user32.dll", SetLastError = true)]
         public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder strText, int maxCount);
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern int GetClassName(IntPtr hWnd, StringBuilder strText, int maxCount);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetShellWindow();
     }
 }
